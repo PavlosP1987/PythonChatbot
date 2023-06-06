7e978e3257cb00ccc149828363294b31027f6d6a
Pavlos Pseftoyiannis <ppseftoyiannis@gmail.com>
2023-06-06 12:09:43 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+$command = escapeshellcmd('python3 /cgi-bin/test1/pip.py');
+$output = shell_exec($command);
+echo $output;
+
+$command1 = escapeshellcmd('python3 /cgi-bin/test1/train.py');
+$output1 = shell_exec($command1);
+echo $output1;
+
+$command2 = escapeshellcmd('python3 /cgi-bin/test1/main.py');
+$output2 = shell_exec($command2);
+echo $output2;
+
+?>
