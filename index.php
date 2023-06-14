f0b87b74cc7d7c76167e52a486ed4b6f910cb952
Pavlos Pseftoyiannis <ppseftoyiannis@gmail.com>
2023-06-14 09:56:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php 
 
-$command = escapeshellcmd('python3 /cgi-bin/test1/pip.py');
+$command = escapeshellcmd('python3 pip.py');
 $output = shell_exec($command);
 echo $output;
 
-$command1 = escapeshellcmd('python3 /cgi-bin/test1/train.py');
+$command1 = escapeshellcmd('python3 train.py');
 $output1 = shell_exec($command1);
 echo $output1;
 
-$command2 = escapeshellcmd('python3 /cgi-bin/test1/main.py');
+$command2 = escapeshellcmd('python3 main.py');
 $output2 = shell_exec($command2);
 echo $output2;
 
-?>
